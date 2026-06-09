93de683e4613fe947c3756cd039e9444280731a7
vaayshaav-stack <vaaysha.av@gmail.com>
2026-06-09 16:24:20 +0530

    day 7 created

--- day5/task5_multiplication.py
+++ day5/task5_multiplication.py
@@ -4,3 +4,4 @@ print(f"--- Multiplication Table of {number} ---")
 for i in range(1, 11):
   result = number * i
   print(f"{number} x {i} = {result}")
+  
---
+++ day7/task2_multiplication_table.py
@@ -0,0 +1,6 @@
+num = int(input("enter number: "))
+for i in range(1,11):
+    print(f"{num} * {i} = {num * i}")
+
+
+  
